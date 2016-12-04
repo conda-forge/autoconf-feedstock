41f488f1d0c725dfd0868973cd4ee8e934b6504d
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-03 23:52:58 -0600

    Rewrite shebang lines so conda can interprete them

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,12 @@ make
 make check
 make install
 
+# replace shebang lines with #!/$PREFIX/bin/perl
+# as conda does not handle the default shebang lines correctly
+FIRST_LINE="#!${PREFIX}/bin/perl"
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/autoheader
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/autom4te
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/autoreconf
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/autoscan
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/autoupdate
+sed -i "1s~.*~${FIRST_LINE}~" $PREFIX/bin/ifnames
